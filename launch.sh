df059bf1bbfcc93d34abed60a07c5699b369c1d9
janisaiad <janisaiad.ja@gmail.com>
2025-12-09 12:39:59 +0100

    refactored whole branch

--- launch.sh
+++ launch.sh
@@ -6,5 +6,4 @@ source .venv/bin/activate
 uv pip install -e .
 uv cache prune
 
-uv run tests/test_env.py
 source .venv/bin/activate
--- tests/test_env.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-## replace with your project folder name
-from project.utils import *
-from project.models import Model1, Model2
